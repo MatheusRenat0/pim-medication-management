92cb0916598ddb6080e17dc07f03faa85861fec3
Matheus Renato <teus.rbe06@gmail.com>
2026-04-01 20:52:22 -0300

    feat: setup vue router and initial landing page structure

--- frontend/medication-frontend/src/App.vue
+++ frontend/medication-frontend/src/App.vue
@@ -1,7 +1,12 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <router-view />
 </template>
+
+<style>
+
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #f9fbfd;
+}
+</style>
